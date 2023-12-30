c7e1c49ef13d95c597b6799f75ba6ce9f498789d
Ak6660 <ayushtyagi4971@gmail.com>
2023-12-31 02:00:57 +0530

    webp

---
+++ src/components/page-partials/Careers/index.jsx
@@ -0,0 +1,11 @@
+import Hero from './Hero';
+import JobList from './JobsList';
+
+const CareersPage = () => {
+    return <>
+    <Hero/>
+    <JobList/>
+    </>;
+};
+
+export default CareersPage;
--- src/data/services.jsx
+++ src/data/services.jsx
@@ -2,22 +2,22 @@ export const services = [
     {
         title: 'API Development',
         description: 'Empower your applications with our robust API development services. Seamless data exchange, integration, and functionality enhancements are just a click away.',
-        icon: 'assets/services/API.png',
+        icon: 'assets/services/API.webp',
     },
     {
         title: 'Application Development',
         description: 'Bring your ideas to life with custom-tailored applications built to meet your specific business needs, ensuring enhanced efficiency and user engagement.',
-        icon: 'assets/services/app-development.png',
+        icon: 'assets/services/app-development.webp',
     },
     {
         title: 'Application Maintenance',
         description: 'Our ongoing maintenance services ensure your applications remain up-to-date, efficient, and bug-free, delivering consistent performance and user satisfaction.',
-        icon: 'assets/services/app-maintenance.png',
+        icon: 'assets/services/app-maintenance.webp',
     },
     {
         title: 'Backup & Disaster Recovery',
         description: 'Guard your vital data and systems against unforeseen disruptions. With our Backup and Disaster Recovery solutions, we ensure business continuity even in critical times.',
-        icon: 'assets/services/backup.png',
+        icon: 'assets/services/backup.webp',
     },
     {
         title: 'Business Process Outsourcing',
@@ -38,94 +38,94 @@ export const services = [
     {
         title: 'Digital Transformation',
         description: 'Redefine and future-proof your business in this digital age. From strategy to execution, our digital transformation services ensure you remain at the forefront of innovation.',
-        icon: 'assets/services/digital-transformation.png',
+        icon: 'assets/services/digital-transformation.webp',
     },
     {
         title: 'Implementation & Deployment',
         description: 'From planning to execution, our experts ensure a seamless and hassle-free implementation and deployment of your software solutions, ensuring quick time-to-market.',
-        icon: 'assets/services/deployment.png',
+        icon: 'assets/services/deployment.webp',
     },
     {
         title: 'Infrastructure Support',
         description: 'Keep your IT infrastructure resilient, updated, and optimized. Our dedicated team provides 24/7 support, ensuring smooth business operations.',
-        icon: 'assets/services/infrastructure.png',
+        icon: 'assets/services/infrastructure.webp',
     },
     {
         title: 'Migrations & Upgrades',
         description: 'Transition to the latest platforms or enhance existing systems. Our migration and upgrade services ensure data integrity, minimal downtime, and optimized performance.',
-        icon: 'assets/services/migrations.png',
+        icon: 'assets/services/migrations.webp',
     },
     {
         title: 'Product Lifecycle Management (PLM)',
         description: 'Manage the entire lifecycle of your products with our PLM services, ensuring streamlined processes from inception to retirement.',
-        icon: 'assets/services/PLM.png',
+        icon: 'assets/services/PLM.webp',
     },
     {
         title: 'Quality Assurance (QA Testing)',
         description: 'Ensure impeccable software quality with our QA services. Rigorous testing methodologies ensure your software is free from glitches and offers optimal performance.',
-        icon: 'assets/services/QA.png',
+        icon: 'assets/services/QA.webp',
     },
     {
         title: 'SLA Support',
         description: 'Dedicated support in line with Service Level Agreements ensures quick resolutions, minimal downtimes, and consistent service quality.',
-        icon: 'assets/services/SLA.png',
+        icon: 'assets/services/SLA.webp',
     },
     {
         title: 'Software Security',
         description:
             'Protect your software assets from vulnerabilities. Our security services focus on identifying and rectifying potential threats, ensuring data protection and application integrity.',
-        icon: 'assets/services/software-security.png',
+        icon: 'assets/services/software-security.webp',
     },
     {
         title: 'Startup & MVP Services',
         description: 'For startups looking to launch fast, our MVP services provide a swift, efficient, and cost-effective route to market, ensuring your idea gets the validation it needs.',
-        icon: 'assets/services/startup.png',
+        icon: 'assets/services/startup.webp',
     },
     {
         title: 'Systems Integration',
         description: 'Streamline your IT landscape by integrating disparate systems into a cohesive unit. Enhance data flow and ensure consistent functionality across the board.',
-        icon: 'assets/services/system-integration.png',
+        icon: 'assets/services/system-integration.webp',
     },
     {
         title: 'Animation & Graphic Design',
         description:
             "Transforming concepts into compelling visuals, our team specializes in creating immersive animations and stunning graphic designs. Catering to diverse sectors, we employ modern tools to illustrate your brand's story effectively.",
-        icon: 'assets/services/graphic.png',
+        icon: 'assets/services/graphic.webp',
     },
     {
         title: 'Artificial Intelligence',
         description:
             'Harness the potential of AI with our specialized solutions. From predictive analytics to machine learning models, we provide intelligent systems tailored to optimize operations and deliver unparalleled user experiences.',
-        icon: 'assets/services/AI.png',
+        icon: 'assets/services/AI.webp',
     },
     {
         title: 'Asset Tracking (RFID & Barcode)',
         description:
             'Streamline asset management with real-time tracking solutions. Utilizing RFID and Barcode technology, we optimize inventory management, reduce asset misplacement, and offer insights into asset lifecycle.',
-        icon: 'assets/services/asset-tracking.png',
+        icon: 'assets/services/asset-tracking.webp',
     },
     {
         title: 'AR / VR',
         description:
             'Dive into immersive experiences with our AR and VR solutions. Suitable for a range of applications, from gaming to real estate, we offer interactive 3D models, virtual environment simulations, and augmented product demos.',
-        icon: 'assets/services/AR.png',
+        icon: 'assets/services/AR.webp',
     },
     {
         title: 'Blockchain',
         description:
             'Safeguard your transactions and data with our blockchain solutions. Focusing on transparency, security, and decentralization, we specialize in smart contracts, cryptocurrency development, and distributed ledger technologies.',
-        icon: 'assets/services/blockchain.png',
+        icon: 'assets/services/blockchain.webp',
     },
     {
         title: 'Business Intelligence & Analytics',
         description:
             'Leverage data-driven insights with our BI and analytics tools. Transform raw data into actionable intelligence, ensuring well-informed decision-making processes and optimized business strategies.',
-        icon: 'assets/services/business-intelligence.png',
+        icon: 'assets/services/business-intelligence.webp',
     },
     {
         title: 'Cloud',
         description:
             'Seamlessly transition to the cloud with our specialized solutions. Offering services across IaaS, PaaS, and SaaS, we ensure scalability, flexibility, and security for all your operations.',
-        icon: 'assets/services/cloud.png',
+        icon: 'assets/services/cloud.webp',
     },
 ];
