13fc9cbd1e6d43fba1ff937e28d87ca7578583f6
MWazidAli <krapton001@gmail.com>
2023-12-30 20:50:40 +0530

    header

--- src/components/common/layout/Header/index.jsx
+++ src/components/common/layout/Header/index.jsx
@@ -15,6 +15,9 @@ const navigation = [
 
 export default function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const onCloseDialog = () =>{
+
+    }
 
     return (
         <header className="bg-white sticky top-0 z-[999]">
@@ -26,7 +29,7 @@ export default function Header() {
                     </a>
                     <div className="hidden lg:flex lg:gap-x-12">
                         {navigation.map((item) => (
-                            <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900 hover:text-primary">
+                            <a key={item.name} href={item.href} className="text-lg font-semibold leading-6 text-gray-900 hover:text-primary">
                                 {item.name}
                             </a>
                         ))}
@@ -46,8 +49,7 @@ export default function Header() {
                     </div>
                 </div>
             </nav>
-            <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                <div className="fixed inset-0 z-20" />
+            <Dialog as="div" className="lg:hidden z-[1000] relative" open={mobileMenuOpen} onClose={onCloseDialog}>
                 <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
                     <div className="flex items-center justify-between">
                         <a href="/" className="-m-1.5 p-1.5 flex sm:hidden">
@@ -63,7 +65,7 @@ export default function Header() {
                         <div className="-my-6 divide-y divide-gray-500/10">
                             <div className="space-y-2 py-6">
                                 {navigation.map((item) => (
-                                    <a key={item.name} href={item.href} className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                                    <a key={item.name} href={item.href} className="-mx-3 block rounded-lg px-3 py-2 text-lg font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                                         {item.name}
                                     </a>
                                 ))}
