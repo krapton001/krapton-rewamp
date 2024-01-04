a4c06927683d8592f4965b55fe43dd374d17e8f1
Suboor Khan <suboork100@gmail.com>
2024-01-05 02:52:15 +0530

    schema setup

---
+++ src/app/api/contact/route.js
@@ -0,0 +1,13 @@
+const schema = {
+    name: 'string',
+    phoneNumber: 'string',
+    message: 'string',
+    location: 'string',
+    email: 'string',
+    purpose: 'string',
+    createdAt: 'date',
+    seen: 'boolean',//default false
+
+}
+
+// create, readAll, delete
