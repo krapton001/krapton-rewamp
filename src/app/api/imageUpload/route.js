6bf5a160027d15ae1f02aefd68630c12cbdf8b99
Suboor Khan <suboork100@gmail.com>
2024-01-07 16:56:11 +0530

    img

--- src/app/api/imageUpload/route.js
+++ src/app/api/imageUpload/route.js
@@ -1,58 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { writeFile } from 'fs/promises';
-import path from 'path';
+import { put } from '@vercel/blob';
+import { NextResponse } from 'next/server';
 
-const fs = require('fs');
-const dir = './public/assets/upload/';
+export async function POST(request) {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get('filename');
 
-/**
- * upload image API that stores images in public folder
- *
- * @param {object} req - The incoming request object.
- * @returns {NextResponse} - The Next.js response object with the blogs data or an error message.
- */
+  const blob = await put(filename, request.body, {
+    access: 'public',
+  });
 
-export async function POST(req) {
-    try {
-        const data = await req.formData();
-        const file = data.get('file');
 
-        if (!file) {
-            return NextRequest.json({
-                code: 204,
-                message: 'No image is uploaded.',
-            });
-        }
-
-        const byteData = await file.arrayBuffer();
-        const buffer = Buffer.from(byteData);
-
-        // Extract the file extension
-        const extension = path.extname(file.name);
-
-        const fileName = `${Date.now()}_${file.name.split(' ')[0]}${extension}`;
-        const dir = './public/assets/upload/';
-        const filePath = path.join(dir, fileName);
-
-        // Ensure the directory exists
-        if (!fs.existsSync(dir)) {
-            fs.mkdirSync(dir, { recursive: true });
-        }
-
-        await writeFile(filePath, buffer);
-
-        const imageUrl = `/assets/upload/${fileName}`;
-
-        return NextResponse.json({
-            status: 200,
-            message: 'File is uploaded successfully',
-            imageUrl,
-        });
-    } catch (e) {
-        return NextResponse.json({
-            status: 500,
-            message: 'Something went wrong!',
-            error: JSON.stringify(e),
-        });
-    }
+  return NextResponse.json(blob);
 }
